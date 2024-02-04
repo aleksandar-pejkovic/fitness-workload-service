65f155dc5bad6080a58f3ab666bc1730486452b8
pejko <aleksandarpejkovic@hotmail.com>
2024-02-04 23:12:13 +0100

    first commit

---
+++ src/test/java/com/example/fitnessworkinghourscalculationservice/FitnessWorkingHoursCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnessworkinghourscalculationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessWorkingHoursCalculationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
